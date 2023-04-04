d76e38e1e3a96f8adc9e833e860e667fc3588ca1
luizfm <luiz.ado@hotmail.com>
2023-04-04 11:35:26 -0300

    feat: project setup

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,9 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, Teste } from '@ignite-ui/react'
+
+export const Primary: StoryObj = {}
+
+export default {
+  title: 'Button',
+  component: Teste,
+} as Meta
---
+++ packages/react/.prettierrc.js
@@ -0,0 +1,3 @@
+const baseConfig = require('@ignite-ui/prettier-config')
+
+module.exports = baseConfig
